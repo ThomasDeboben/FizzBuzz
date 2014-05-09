5c3611272c85a33852ce210e93b532fdca729d57
Dirk Kuypers <Dirk@Kuypers.de>
2014-05-09 16:46:41 +0200

    Added unit test project

---
+++ FizzBuzzConsole/FizzBuzzLibTests/FizzBuzzTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FizzBuzzLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace FizzBuzzLib.Tests
+{
+    [TestClass()]
+    public class FizzBuzzTests
+    {
+        [TestMethod()]
+        public void IsValueMultipleFromDividerTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
