1c46a8ae1ff744b88b2984729679d890970cdf16
Thomas Deboben <t@deboben.de>
2014-05-09 14:38:56 +0200

    style changes

--- FizzBuzzConsole/Program.cs
+++ FizzBuzzConsole/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FizzBuzzConsole
+﻿namespace FizzBuzzConsole
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     class Program
     {
         static void Main(string[] args)
