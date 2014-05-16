37b7d8b447dbe0cb797568a1c511509e11ca83cb
Thomas Deboben <t@deboben.de>
2014-05-16 15:23:59 +0200

    add wpf listbox and add fizzbuzz formated items

--- FizzBuzzWpf/MainWindow.xaml.cs
+++ FizzBuzzWpf/MainWindow.xaml.cs
@@ -1,40 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using FizzBuzzLib;
-
-namespace FizzBuzzWpf
+﻿namespace FizzBuzzWpf
 {
+    using System.Windows;
+
+    using FizzBuzzLib;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
 
+        /// <summary>
+        /// The run fizz buzz.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event argument.</param>
         private void RunFizzBuzz(object sender, RoutedEventArgs e)
         {
             var fizzbuzz = new FizzBuzz();
 
             for (int i = 1; i <= 100; i++)
             {
-
+                var result = fizzbuzz.GetFizzBuzzFormattedString(i);
+                this.FizzBuzzListBox.Items.Add(result);
             }
-
         }
     }
 }
