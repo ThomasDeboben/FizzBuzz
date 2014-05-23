de39ecbb23a54e5446b9f58680bb14aea905d983
Thomas Deboben <t@deboben.de>
2014-05-23 15:22:08 +0200

    Change unit tests to NUnit

--- FizzBuzzConsole/FizzBuzzLibTests/FizzBuzzTests.cs
+++ FizzBuzzConsole/FizzBuzzLibTests/FizzBuzzTests.cs
@@ -1,62 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FizzBuzzLib;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MSTestHacks;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FizzBuzzTests.cs" company="FizzBuzz">
+// Copyright 2014  
+// </copyright>
+// <summary>
+//   The fizz buzz tests.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
-namespace FizzBuzzLib.Tests
+namespace FizzBuzzLibTests
 {
-    [TestClass()]
+    using FizzBuzzLib;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// The fizz buzz tests.
+    /// </summary>
     public class FizzBuzzTests
     {
-        public IEnumerable<int> TestNumbers
-        {
-            get
-            {
-                //This could do anything, fetch a dynamic list from anywhere....
-                return new List<int> { 1, 2, 3 };
-            }
-        }
-
-        [TestMethod()]
-        public void IsValueMultipleFromThreeTest()
+        []
+        public void FizzBuzzTest(int value, string expectedResult)
         {
             var fizzBuzz = new FizzBuzz();
-            Assert.IsTrue(fizzBuzz.IsValueMultipleFromDivider(3, 3), "number is no multiple of divider");
-            Assert.IsTrue(fizzBuzz.IsValueMultipleFromDivider(6, 3), "number is no multiple of divider");
-            Assert.IsTrue(fizzBuzz.IsValueMultipleFromDivider(9, 3), "number is no multiple of divider");
-            Assert.IsTrue(fizzBuzz.IsValueMultipleFromDivider(12, 3), "number is no multiple of divider");
-            Assert.IsTrue(fizzBuzz.IsValueMultipleFromDivider(15, 3), "number is no multiple of divider");
-            Assert.IsFalse(fizzBuzz.IsValueMultipleFromDivider(1, 3), "number is no multiple of divider");
-            Assert.IsFalse(fizzBuzz.IsValueMultipleFromDivider(2, 3), "number is no multiple of divider");
-            Assert.IsFalse(fizzBuzz.IsValueMultipleFromDivider(14, 3), "number is no multiple of divider");
+            Assert.AreEqual(fizzBuzz.IsValueMultipleFromDivider(i, 3));
         }
 
-        [TestMethod()]
-        public void IsValueMultipleFromFiveTest()
+        /// <summary>
+        /// The is value multiple from divider test.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="divider">The divider.</param>
+        /// <param name="expectedResult">The expected Result.</param>
+        [TestCase(1, 3, false)]
+        [TestCase(2, 3, false)]
+        [TestCase(3, 3, true)]
+        [TestCase(4, 3, false)]
+        [TestCase(5, 3, false)]
+        [TestCase(6, 3, true)]
+        [TestCase(9, 3, true)]
+        [TestCase(12, 3, true)]
+        [TestCase(15, 3, true)]
+        [TestCase(18, 3, true)]
+        [TestCase(1, 5, false)]
+        [TestCase(2, 5, false)]
+        [TestCase(3, 5, false)]
+        [TestCase(4, 5, false)]
+        [TestCase(5, 5, true)]
+        [TestCase(6, 5, false)]
+        [TestCase(9, 5, false)]
+        [TestCase(10, 5, true)]
+        [TestCase(15, 5, true)]
+        [TestCase(20, 5, true)]
+        public void IsValueMultipleFromDividerTest(int value, int divider, bool expectedResult)
         {
             var fizzBuzz = new FizzBuzz();
-            Assert.IsTrue(fizzBuzz.IsValueMultipleFromDivider(5, 5), "number is no multiple of divider");
-            Assert.IsTrue(fizzBuzz.IsValueMultipleFromDivider(10, 5), "number is no multiple of divider");
-            Assert.IsTrue(fizzBuzz.IsValueMultipleFromDivider(15, 5), "number is no multiple of divider");
-            Assert.IsTrue(fizzBuzz.IsValueMultipleFromDivider(20, 5), "number is no multiple of divider");
-            Assert.IsFalse(fizzBuzz.IsValueMultipleFromDivider(1, 5), "number is no multiple of divider");
-            Assert.IsFalse(fizzBuzz.IsValueMultipleFromDivider(2, 5), "number is no multiple of divider");
-            Assert.IsFalse(fizzBuzz.IsValueMultipleFromDivider(14, 5), "number is no multiple of divider");
+            Assert.AreEqual(expectedResult, fizzBuzz.IsValueMultipleFromDivider(value, divider));
         }
 
-        [TestMethod()]
-        public void ValueContainsDigitTest()
+        /// <summary>
+        /// The does value contains a digit test.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="digit">The digit.</param>
+        /// <param name="expectedResult">The expected Result.</param>
+        [TestCase(1, 3, false)]
+        [TestCase(2, 3, false)]
+        [TestCase(3, 3, true)]
+        [TestCase(13, 3, true)]
+        [TestCase(35, 3, true)]
+        [TestCase(51, 5, true)]
+        public void ValueContainsDigitTest(int value, int digit, bool expectedResult)
         {
             var fizzBuzz = new FizzBuzz();
-            Assert.IsTrue(fizzBuzz.ValueContainsDigit(5, 5), "value does not contain digit");
-            Assert.IsTrue(fizzBuzz.ValueContainsDigit(15, 5), "value does not contain digit");
-            Assert.IsTrue(fizzBuzz.ValueContainsDigit(25, 5), "value does not contain digit");
-            Assert.IsTrue(fizzBuzz.ValueContainsDigit(35, 5), "value does not contain digit");
-            Assert.IsFalse(fizzBuzz.ValueContainsDigit(10, 5), "value does contain digit");
+            Assert.AreEqual(expectedResult, fizzBuzz.ValueContainsDigit(value, digit));
         }
     }
 }
